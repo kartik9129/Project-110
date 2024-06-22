5a6556da11b23336526f453b98c7e53129ab8138
kartik9129 <133895591+kartik9129@users.noreply.github.com>
2024-06-22 13:52:29 +0530

    Add files via upload

---
+++ Project-110/roll-a-dice.py
@@ -0,0 +1,83 @@
+import random
+
+no = "y"
+
+while no == "y":
+    no = random.randint(1,6)
+
+if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+elif no == 2:
+    print("[-----]")
+    print("[     ]")
+    print("[0   0]")
+    print("[     ]")
+    print("[-----]")
+elif no == 3:
+    print("[-----]")
+    print("[0    ]")
+    print("[  0  ]")
+    print("[    0]")
+    print("[-----]")
+elif no == 4:
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+elif no == 5:
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+elif no == 6:
+    print("[-----]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[-----]")
+response = str(input("Press y to roll again and n to exit:"))
+
+if no == "y":
+   if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+   elif no == 2:
+    print("[-----]")
+    print("[     ]")
+    print("[0   0]")
+    print("[     ]")
+    print("[-----]")
+   elif no == 3:
+    print("[-----]")
+    print("[0    ]")
+    print("[  0  ]")
+    print("[    0]")
+    print("[-----]")
+   elif no == 4:
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+   elif no == 5:
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+   elif no == 6:
+    print("[-----]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[-----]")
+   response = str(input("Press y to roll again and n to exit:"))
